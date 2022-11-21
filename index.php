5f968fe2ddc79d4c5cd68c04a004d32aabb63bc1
remoflury <remoflury97+web@gmail.com>
2022-11-21 21:44:08 +0100

    favicon

--- index.php
+++ index.php
@@ -7,6 +7,9 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="src/assets/favicon/Favicon WorkWise_v2.svg">
   
   <!-- CSS -->
   <link rel="stylesheet" href="css/output.css">
