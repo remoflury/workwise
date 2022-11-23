c62d11801a2344a528e1698fcbe0461dbb755a44
remoflury <remoflury97+web@gmail.com>
2022-11-23 18:09:46 +0100

    added vorlage

---
+++ _vorlage.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<html lang="de">
+<head>
+  <!-- Meta Tags -->
+  <?php include 'src/layout/htmlHead/metaTags.php';?>
+
+  <!-- Links (CSS, JS usw.) -->
+  <?php include 'src/layout/htmlHead/htmlLinks.php';?>
+
+
+  <title>Workwise</title>
+</head>
+  <body>
+
+    <!-- Header -->
+    <?php include 'src/layout/header.php';?>
+
+    <div class="container">
+      
+    </div>
+
+    <!-- Footer -->
+    <?php include 'src/layout/footer.php';?>
+
+  </body>
+</html>
