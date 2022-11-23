c8370323a8b2735c2463e81b1ebfbf8af4e39286
remoflury <remoflury97+web@gmail.com>
2022-11-23 14:04:54 +0100

    restructuring of php

--- index.php
+++ index.php
@@ -4,41 +4,34 @@
 
 <html lang="de">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- Meta Tags -->
+  <?php include 'src/layout/htmlHead/metaTags.php';?>
 
-  <!-- Favicon -->
-  <link rel="icon" type="image/x-icon" href="src/assets/favicon/Favicon WorkWise_v2.svg">
-  
-  <!-- CSS -->
-  <link rel="stylesheet" href="css/output.css">
-  
-  <!-- JS -->
-  <script src="js/main.js" defer></script>
+  <!-- Links (CSS, JS usw.) -->
+  <?php include 'src/layout/htmlHead/htmlLinks.php';?>
 
   <title>Workwise</title>
 </head>
-<body>
+  <body>
 
-  <!-- Header -->
-  <?php include 'src/layout/header.php';?>
+    <!-- Header -->
+    <?php include 'src/layout/header.php';?>
 
-  <div class="container">
-    <h1>Heading 1</h1>
-    <h2>Heading 2</h2>
-    <h3>Heading 3</h3>
-    <h4>Heading 4</h4>
-    <h5>Heading 5</h5>
-    <h6>Heading 6</h6>
+    <div class="container">
+      <h1>Heading 1</h1>
+      <h2>Heading 2</h2>
+      <h3>Heading 3</h3>
+      <h4>Heading 4</h4>
+      <h5>Heading 5</h5>
+      <h6>Heading 6</h6>
 
 
-    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae mollitia porro iusto alias at. Pariatur cupiditate nobis doloribus consequuntur cumque. Eaque consectetur similique harum maxime! Ad nobis sint delectus quo?</p>
-  </div>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae mollitia porro iusto alias at. Pariatur cupiditate nobis doloribus consequuntur cumque. Eaque consectetur similique harum maxime! Ad nobis sint delectus quo?</p>
+    </div>
 
-  <!-- Footer -->
-  <?php include 'src/layout/footer.php';?>
+    <!-- Footer -->
+    <?php include 'src/layout/footer.php';?>
 
-  
-</body>
+    
+  </body>
 </html>
