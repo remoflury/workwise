7526d6d5227fde3ba659709565583e8c94a99c5a
remoflury <remoflury97+web@gmail.com>
2023-01-10 10:47:33 +0100

    comments registration

--- backend/registration.php
+++ backend/registration.php
@@ -12,22 +12,28 @@ $email = $_POST["email"];
 $username = $_POST["username"];
 $password = $_POST["password"];
 
+// Passwort verschlüsseln
+$passwordHashed = password_hash($password, PASSWORD_DEFAULT);
+
 // TODO: check if user already exist
 
+// Statement um Daten in DB einzufügen
 $sql = "INSERT INTO users (username, email, password) VALUES (:Username, :Email, :Password)";
 
 $stmt = $pdo->prepare($sql);
 
-$erfolg = $stmt->execute(array('Username' => $username, 'Email' => $email, 'Password' => $password));
+// Statement ausführen.
+$erfolg = $stmt->execute(array('Username' => $username, 'Email' => $email, 'Password' => $passwordHashed));
 
+// wenn Ausführung erfolgreich, dann...
 if ($erfolg) {
-
     echo '{
       "error": false,
       "message": "Registrierung erfolgreich"
     }';
-} else {
-
+} 
+// wenn Ausführung nicht erfolgreich, dann...
+else {
     echo '{
       "error": true,
       "message": "Ups, da lief etwas schief. Bitte versuche es erneut."
