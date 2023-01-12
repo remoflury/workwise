4fd7aeb1b0f83736b356a476d6675a7972590155
remoflury <remoflury97+web@gmail.com>
2023-01-12 10:48:51 +0100

    .gitignore fix

--- backend/rentWorkspace.php
+++ backend/rentWorkspace.php
@@ -14,7 +14,16 @@ $stmt = $pdo->prepare($sql);
 $success = $stmt->execute();
 
 if ($success) {
-  // $result = 
+  $result = $stmt->fetchAll();
+  $resultCount = count($result);
+
+  // Wenn kein Resultat mit dieser ID vorhanden
+  if ($resultCount !== 1) {
+    echo '{
+      "error": true,
+      "message": "Ups, da lief etwas schief. Bitte versuche es später noch einmal."
+    }';
+  }
   
 } else {
   echo '{
