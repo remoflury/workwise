392317e145c796532ba94764c20e215ec6c69e03
remoflury <remoflury97+web@gmail.com>
2023-01-13 15:02:36 +0100

    accessability btns

--- src/components/buttons/primaryButton.php
+++ src/components/buttons/primaryButton.php
@@ -1,5 +1,5 @@
 <a href="<?php echo $link ?>" class="group" title="<?php echo $btnText ?>">
-  <button class="border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block group-hover:bg-white transition">
-    <p class="font-bold text-white group-hover:text-orange-dark transition"><?php echo $btnText ?></p>
+  <button class="border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block group-hover:bg-white font-bold text-white group-hover:text-orange-dark transition">
+    <?php echo $btnText ?>
   </button>
 </a>
--- src/components/buttons/primaryButtonSubmit.php
+++ src/components/buttons/primaryButtonSubmit.php
@@ -1,4 +1,4 @@
 
-<button onclick="<?php echo $onClickFunction?>" class="group border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block hover:bg-white transition">
-  <p class="font-bold text-white group-hover:text-orange-dark transition"><?php echo $btnText ?></p>
+<button onclick="<?php echo $onClickFunction?>" class="group border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block hover:bg-white font-bold text-white hover:text-orange-dark transition">
+  <?php echo $btnText ?>
 </button>
