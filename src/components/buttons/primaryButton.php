293bbf9f5da76090d32cfbb1c19001d5bc6a1d4b
remoflury <remoflury97+web@gmail.com>
2023-01-13 15:10:07 +0100

    accessability btns

--- src/components/buttons/primaryButton.php
+++ src/components/buttons/primaryButton.php
@@ -1,5 +1,7 @@
-<a href="<?php echo $link ?>" class="group" title="<?php echo $btnText ?>">
-  <button class="border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block group-hover:bg-white font-bold text-white group-hover:text-orange-dark transition">
-    <?php echo $btnText ?>
-  </button>
+<a 
+  href="<?php echo $link ?>" 
+  class="border-2 border-orange-dark bg-orange-dark px-16 py-4 inline-block hover:bg-white font-bold text-white text-base md:text-lg xl:text-xl hover:text-orange-dark transition"
+  title="<?php echo $btnText ?>"
+  role="button">
+  <?php echo $btnText ?>
 </a>
